03df2af5674d3644c824fc7df951c6bc86cb33b5
kevin9010 <xufashu@qq.com>
2022-10-28 13:54:55 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -206,14 +206,14 @@ def json_to_md(filename,md_filename,
             f.write(f"<p align=right>(<a href={top_info}>back to top</a>)</p>\n\n")
         
         if show_badge == True:
-            f.write(f"[contributors-shield]: https://img.shields.io/github/contributors/Vincentqyw/cv-arxiv-daily.svg?style=for-the-badge\n")
-            f.write(f"[contributors-url]: https://github.com/Vincentqyw/cv-arxiv-daily/graphs/contributors\n")
-            f.write(f"[forks-shield]: https://img.shields.io/github/forks/Vincentqyw/cv-arxiv-daily.svg?style=for-the-badge\n")
-            f.write(f"[forks-url]: https://github.com/Vincentqyw/cv-arxiv-daily/network/members\n")
-            f.write(f"[stars-shield]: https://img.shields.io/github/stars/Vincentqyw/cv-arxiv-daily.svg?style=for-the-badge\n")
-            f.write(f"[stars-url]: https://github.com/Vincentqyw/cv-arxiv-daily/stargazers\n")
-            f.write(f"[issues-shield]: https://img.shields.io/github/issues/Vincentqyw/cv-arxiv-daily.svg?style=for-the-badge\n")
-            f.write(f"[issues-url]: https://github.com/Vincentqyw/cv-arxiv-daily/issues\n\n")
+            f.write(f"[contributors-shield]: https://img.shields.io/github/contributors/kevin9010/exo-arxiv-daily.svg?style=for-the-badge\n")
+            f.write(f"[contributors-url]: https://github.com/kevin9010/exo-arxiv-daily/graphs/contributors\n")
+            f.write(f"[forks-shield]: https://img.shields.io/github/forks/kevin9010/exo-arxiv-daily.svg?style=for-the-badge\n")
+            f.write(f"[forks-url]: https://github.com/kevin9010/exo-arxiv-daily/network/members\n")
+            f.write(f"[stars-shield]: https://img.shields.io/github/stars/kevin9010/exo-arxiv-daily.svg?style=for-the-badge\n")
+            f.write(f"[stars-url]: https://github.com/kevin9010/exo-arxiv-daily/stargazers\n")
+            f.write(f"[issues-shield]: https://img.shields.io/github/issues/kevin9010/exo-arxiv-daily.svg?style=for-the-badge\n")
+            f.write(f"[issues-url]: https://github.com/kevin9010/exo-arxiv-daily/issues\n\n")
                 
     print("finished")        
 
