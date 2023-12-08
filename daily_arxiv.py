803e064c052268942d12de65c2aac790005548f1
kevin9010 <fashuxu@163.com>
2023-12-08 10:34:06 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -225,12 +225,14 @@ if __name__ == "__main__":
     data_collector_web= []
     
     keywords = dict()
-    keywords["SLAM"]                = "SLAM"
-    keywords["SFM"]                 = "SFM"+"OR"+"\"Structure from Motion\""
-    keywords["Visual Localization"] = "\"Camera Localization\"OR\"Visual Localization\"OR\"Camera Re-localisation\"OR\"Loop Closure Detection\"OR\"visual place recognition\"OR\"image retrieval\""
-    keywords["Keypoint Detection"]  = "\"Keypoint Detection\"OR\"Feature Descriptor\""
-    keywords["Image Matching"]      = "\"Image Matching\"OR\"Keypoint Matching\""
-    keywords["NeRF"]                = "NeRF"
+    # keywords["SLAM"]                = "SLAM"
+    # keywords["SFM"]                 = "SFM"+"OR"+"\"Structure from Motion\""
+    # keywords["Visual Localization"] = "\"Camera Localization\"OR\"Visual Localization\"OR\"Camera Re-localisation\"OR\"Loop Closure Detection\"OR\"visual place recognition\"OR\"image retrieval\""
+    # keywords["Keypoint Detection"]  = "\"Keypoint Detection\"OR\"Feature Descriptor\""
+    # keywords["Image Matching"]      = "\"Image Matching\"OR\"Keypoint Matching\""
+    # keywords["NeRF"]                = "NeRF"
+    keywords["Exosuit"]                = "Exosuit"
+    keywords["Exoskeleton"]                = "Exoskeleton"
 
     for topic,keyword in keywords.items():
  
